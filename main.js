3938bc7bc2248470cc970b62630b0e25575e5fa5
cindykandie <cindyjk6@gmail.com>
2023-04-03 14:54:51 +0300

    Polish Js interactivity on the rating buttons on submit

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
-let ratingValue = document.querySelector('li')
-let submitBtn = document.querySelector('.submit-btn')
-let rating = 0
-let parseRating = () => {
-   return rating = ratingValue.innerText
+const mainSection = document.querySelector('.section_one')
+const thankyouSection = document.querySelector('.thankyou-container')
+
+const ratingCard = document.querySelector('.rating-btns')
+const ratingValues = document.querySelectorAll('.rating-no')
+
+const submitBtn = document.querySelector('.submit-btn')
+const pickedRating = document.querySelector('.display-rating')
+let rating;
+
+let parseRating = (e) => {
+    ratingValues.forEach(item => item.classList.remove('active-rate'))
+    e.target.classList.add('active-rate')
+    rating = e.target.innerText
 }
 let getRating = (e) => {
     e.preventDefault
-    console.log(rating)
+    mainSection.style.display = "none"
+    thankyouSection.style.display = "flex"
+    pickedRating.innerText = `${rating}`
 }
+ratingCard.addEventListener('click', parseRating);
+
 submitBtn.addEventListener('click', getRating)
-ratingValue.addEventListener('click', parseRating)
